fad35b4ba580d29ecca46b2d270bf4b583a766e8
Ithos <kerumanu@hotmail.com>
2025-10-26 23:33:51 +0100

    Small optimizations and warning fixes

--- src/Components/SearchResults.js
+++ src/Components/SearchResults.js
@@ -1,16 +1,19 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback, useMemo } from "react";
 import axios from "axios";
 
 const SearchResults = ({ cardSearchObject, bannedCards, cardsPerPage, scrollPosition }) => {
 
+    const bannedCardsString = JSON.stringify(bannedCards);
+    const memoizedBannedCards = useMemo(() => { return bannedCardsString; }, [bannedCardsString]);
+
     const [currentPage, setCurrentPage] = useState(1);
     const [pageUrl, setPageUrl] = useState(cardSearchObject.hasOwnProperty('next_page') ? cardSearchObject.next_page : null);
-    const [filteredCards, setFilteredCards] = useState([...cardSearchObject.data.filter((card) => !bannedCards.includes(card.name))]);
+    const [filteredCards, setFilteredCards] = useState([...cardSearchObject.data.filter((card) => !memoizedBannedCards.includes(card.name))]);
     const [isLoading, setIsLoading] = useState(true);
 
     const maxPage = Math.ceil(cardSearchObject.total_cards / cardsPerPage);
 
-    async function adjustPage() {
+    const adjustPage = useCallback( async function () {
         setIsLoading(true);
         if (pageUrl) {
             const currentPageMatch = pageUrl.match(/&page=(\d+)/);
@@ -20,11 +23,11 @@ const SearchResults = ({ cardSearchObject, bannedCards, cardsPerPage, scrollPosi
                     let result = await axios.get(tmpPage)
                         .catch(err => console.error(err))
                         .then(res => (res && res.data) ? res.data : null);
-                    setFilteredCards([...result.data.filter((card) => !bannedCards.includes(card.name))]);
+                    setFilteredCards([...result.data.filter((card) => !memoizedBannedCards.includes(card.name))]);
             }
         }
         setIsLoading(false);
-    };
+    } , [currentPage, maxPage, pageUrl, memoizedBannedCards]);
 
     useEffect(() => {
         setPageUrl(cardSearchObject.hasOwnProperty('next_page') ? cardSearchObject.next_page : null);
@@ -34,14 +37,13 @@ const SearchResults = ({ cardSearchObject, bannedCards, cardsPerPage, scrollPosi
     
     useEffect(() => {
         adjustPage();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [currentPage]);
+    }, [adjustPage]);
 
 
     return (
         <section className="searchResults" id="search-results">
             <section className="resultsHeader">
-                <span> Page {currentPage} of {maxPage} &#9148; {maxPage > 1 ? cardSearchObject.total_cards : filteredCards.length - filteredCards.filter((card) => bannedCards.includes(card.name)).length} results found</span>
+                <span> Page {currentPage} of {maxPage} &#9148; {maxPage > 1 ? cardSearchObject.total_cards : filteredCards.length - filteredCards.filter((card) => memoizedBannedCards.includes(card.name)).length} results found</span>
                 <section className="resultsNavigation">
                     <button className= { currentPage > 1 ? "button firstPage" : "disabled firstPage" } onClick={() => { if (currentPage > 1) setCurrentPage(1) }} title="First Page"> &laquo;</button>
                     <button className={ currentPage > 1 ? "button prevPage" : "disabled prevPage" } onClick={() => { if (currentPage > 1) setCurrentPage(currentPage - 1) }} title="Previous Page"> &lsaquo; Prev</button>
@@ -52,7 +54,7 @@ const SearchResults = ({ cardSearchObject, bannedCards, cardsPerPage, scrollPosi
             { scrollPosition > 3 && <button className="ms ms-untap scrollToTop" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} title="Scroll to Top"></button> }
             <section className="cardsContainer">
             { 
-                !isLoading && filteredCards.map( (card) => (
+                !isLoading && filteredCards.length > 0 && filteredCards.map( (card) => (
                     <div className={`card`} key={card.id}>
                         <a className="cardImageLink" href={`https://scryfall.com/card/${card.id}`} target="_blank" rel="noopener noreferrer">
                             <img className="searchCardImage" src={card.image_uris ? card.image_uris.normal : ( card.card_faces ? card.card_faces[0].image_uris.normal : '' )} loading='lazy' alt={card.name} title={card.name} />
@@ -60,6 +62,7 @@ const SearchResults = ({ cardSearchObject, bannedCards, cardsPerPage, scrollPosi
                     </div>
                 ))
             }
+            { filteredCards.length === 0 && !isLoading && <div className="no-results">No results found</div> }
             { isLoading && <section>
                                 <div className="loading">Loading...</div>
                                 <div className="dots-loading" aria-label="Loading" role="status">
@@ -71,7 +74,7 @@ const SearchResults = ({ cardSearchObject, bannedCards, cardsPerPage, scrollPosi
             }
             </section>
                 <section className="resultsHeader">
-                <span> Page {currentPage} of {maxPage} &#9148; {maxPage > 1 ? cardSearchObject.total_cards : filteredCards.length - filteredCards.filter((card) => bannedCards.includes(card.name)).length} results found</span>
+                <span> Page {currentPage} of {maxPage} &#9148; {maxPage > 1 ? cardSearchObject.total_cards : filteredCards.length - filteredCards.filter((card) => memoizedBannedCards.includes(card.name)).length} results found</span>
                 <section className="resultsNavigation">
                     <button className= { currentPage > 1 ? "button firstPage" : "disabled firstPage" } onClick={() => { if (currentPage > 1) setCurrentPage(1) } } title="First Page"> &laquo;</button>
                     <button className={ currentPage > 1 ? "button prevPage" : "disabled prevPage" } onClick={() => { if (currentPage > 1) setCurrentPage(currentPage - 1) }} title="Previous Page"> &lsaquo; Prev</button>
